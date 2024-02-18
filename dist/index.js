82b44b6bc1b7bec94adba58c4f426e8d2694d3d7
Matheus Ferreira <theliomatheus@protonmail.com>
2024-02-18 05:56:08 -0300

    Remove IsCPF export from index.js

--- dist/index.js
+++
@@ -1,5 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.IsCPF = void 0;
-var IsCPF_1 = require("./IsCPF");
-Object.defineProperty(exports, "IsCPF", { enumerable: true, get: function () { return IsCPF_1.IsCPF; } });
